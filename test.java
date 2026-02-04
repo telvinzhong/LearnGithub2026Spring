dfa13bd53472bd9b372789a84eccda9383bfb80a
Jesse <jesse.olufade@sjsu.edu>
2026-02-03 19:48:00 -0800

    test

---
+++ test.java
@@ -0,0 +1,12 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+        int num1 = 2;
+        int num2 = 2;
+
+        int yes = num1 + num2;
+        System.out.printf("The answer is " + yes);
+        System.out.println("bruh");
+    }
+}
